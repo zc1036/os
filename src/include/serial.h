fda8502e702681b34eb12885d14f47ea105c0717
Joseph Cavazos <Joseph.C.Cavazos@gmail.com>
2020-06-26 22:30:28 -0500

    Adding guards to serial.h and moving OPT_EXTERN definition into stddef.h. #2

--- src/include/serial.h
+++ src/include/serial.h
@@ -1,11 +1,10 @@
+#include <stddef.h>
+
+#ifndef SERIAL_H
+#define SERIAL_H 1
+
 extern unsigned int serial_enabled;
 extern unsigned int serial_initialized;
-
-#ifdef __cplusplus
-#define OPT_EXTERN extern "C"
-#else
-#define OPT_EXTERN
-#endif
 
 OPT_EXTERN void serial_initialize();
 OPT_EXTERN int serial_received();
@@ -14,3 +13,5 @@ OPT_EXTERN int serial_is_transmit_empty();
 OPT_EXTERN void serial_putchar(char c);
 OPT_EXTERN void serial_write(const char* data, size_t size);
 OPT_EXTERN void serial_writestring(const char* data);
+
+#endif
--- src/include/stddef.h
+++ src/include/stddef.h
@@ -2,7 +2,16 @@
 #ifndef STDDEF_H
 #define STDDEF_H
 
+#ifdef __cplusplus
+#define OPT_EXTERN extern "C"
+#else
+#define OPT_EXTERN
+#endif
+
 typedef unsigned long size_t;
 typedef long ptrdiff_t;
 
 #endif
+extern unsigned int serial_enabled;
+extern unsigned int serial_initialized;
+
